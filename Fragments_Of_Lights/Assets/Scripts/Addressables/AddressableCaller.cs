95743501e76ea6091ba5a48bf22a382edfb0fb03
Shakedev96 <99559163+Shakedev96@users.noreply.github.com>
2025-04-02 03:08:36 +0530

    level Loading in progress

---
+++ Fragments_Of_Lights/Assets/Scripts/Addressables/AddressableCaller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AddressableCaller : MonoBehaviour
+{
+    [SerializeField] AssetReferenceGameObject _ptOne;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))   
+        {
+            _ptOne.InstantiateAsync();
+        }
+    }
+
+    void OnaddressableLoaded(AsyncOperationHandle<GameObject> handle)
+    {
+        if(handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            Instantiate(handle.Result);
+        }
+        else
+        {
+            Debug.Log("Map Loading Fail");
+        }
+    }
+}
