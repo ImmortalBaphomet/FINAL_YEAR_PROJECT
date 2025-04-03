e965b613fd4f1efe9f8a56e66cc40ec69de162c9
4manfear <rickroad774@gmail.com>
2025-04-03 14:33:55 +0530

    Comit Changes made

--- Fragments_Of_Lights/Assets/Scripts/Scripts_For_UI/Title_Screen.cs
+++ Fragments_Of_Lights/Assets/Scripts/Scripts_For_UI/Title_Screen.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class Title_Screen : MonoBehaviour
 {
-    public float delay = 3f; 
-
+    public float delay = 3f;
+    public VideoPlayer videoPlayer; // Reference to the VideoPlayer
+    public GameObject Start_Screen;
     private bool isTransitioning = false;
+
+    private void Start()
+    {
+        Start_Screen.SetActive(true);
+        videoPlayer.playOnAwake = false; // Ensure the video doesn't play automatically
+    }
 
     void Update()
     {
         if (Input.anyKeyDown && !isTransitioning)
         {
-            StartCoroutine(ChangeSceneWithDelay());
+            Start_Screen.SetActive(false);
+            videoPlayer.Play();
+            StartCoroutine(PlayVideoAndChangeScene());
         }
     }
 
-    private IEnumerator ChangeSceneWithDelay()
+    private IEnumerator PlayVideoAndChangeScene()
     {
         isTransitioning = true; 
         yield return new WaitForSeconds(delay); 
