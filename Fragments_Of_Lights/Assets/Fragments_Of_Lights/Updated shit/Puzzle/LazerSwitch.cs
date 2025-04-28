efcf783f845fafa4f0219ab2179d160ea9a58094
Shakedev96 <99559163+Shakedev96@users.noreply.github.com>
2025-04-28 22:43:48 +0530

    fix1

--- Fragments_Of_Lights/Assets/Fragments_Of_Lights/Updated shit/Puzzle/LazerSwitch.cs
+++ Fragments_Of_Lights/Assets/Fragments_Of_Lights/Updated shit/Puzzle/LazerSwitch.cs
@@ -4,6 +4,81 @@ using UnityEngine.InputSystem;
 public class LazerSwitch : MonoBehaviour
 {
     [Header("Laser Settings")]
+    public string laserObjectName = "LaserBeam(Clone)";
+    public string laserTag = "Laser";
+    private LazerBeam laserToActivate;
+
+    [Header("Interaction Settings")]
+    public float interactionRange = 3f;
+    public Transform interactionOrigin;
+
+    [Header("Input")]
+    public InputActionReference activateAction; // <-- made public
+
+    private void OnEnable()
+    {
+        if (activateAction != null)
+            activateAction.action.performed += OnActivateLaser;
+    }
+
+    private void OnDisable()
+    {
+        if (activateAction != null)
+            activateAction.action.performed -= OnActivateLaser;
+    }
+
+    private void Update()
+    {
+        if (laserToActivate == null)
+        {
+            FindLaser();
+        }
+    }
+
+    private void FindLaser()
+    {
+        GameObject laserObj = GameObject.Find(laserObjectName);
+
+        if (laserObj != null)
+        {
+            laserToActivate = laserObj.GetComponent<LazerBeam>();
+            Debug.Log("Laser found and ready to activate.");
+        }
+    }
+
+    public void OnActivateLaser(InputAction.CallbackContext context)
+    {
+        if (laserToActivate != null)
+        {
+            float distance = Vector3.Distance(
+                interactionOrigin != null ? interactionOrigin.position : transform.position,
+                laserToActivate.transform.position
+            );
+
+            if (distance <= interactionRange)
+            {
+                laserToActivate.ActivateLaser();
+                Debug.Log("Laser activated by player input.");
+            }
+            else
+            {
+                Debug.Log("Too far from the laser to activate.");
+            }
+        }
+        else
+        {
+            Debug.Log("No laser found to activate.");
+        }
+    }
+}
+
+
+
+//old logic
+
+/*
+
+[Header("Laser Settings")]
     public string laserObjectName = "LaserBeam(Clone)"; // name of the laser prefab when instantiated
     public string laserTag = "Laser"; // optional tag if you want to use tags instead
     private LazerBeam laserToActivate;
@@ -81,4 +156,5 @@ public class LazerSwitch : MonoBehaviour
             Debug.Log("No laser found to activate.");
         }
     }
-}
+
+*/
